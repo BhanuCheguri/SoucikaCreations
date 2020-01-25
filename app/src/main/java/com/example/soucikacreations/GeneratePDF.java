0e302158accaaa87c0c4916806bce21084e4d69f
bhanupriyacheguri@gmail.com <Secureme@123>
2020-01-25 21:45:18 +0530

    Initial commit

---
+++ app/src/main/java/com/example/soucikacreations/GeneratePDF.java
@@ -0,0 +1,14 @@
+package com.example.soucikacreations;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GeneratePDF extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generate_pdf);
+    }
+}
